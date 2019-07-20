c7949ffb56523b7c9d96d9e6d0060f7d2ee868e7
Tran Quoc Hung <mw3tv123@gmail.com>
2019-07-20 22:26:08 +0700

    Added Player's movement by click to point method (RayCasting).

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour {
+    NavMeshAgent agent;     // Reference to this object's NavMeshAgent for navigating/moving object around.
+
+    Ray controller_ray;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+            Move();
+    }
+
+    private void Move() {
+        controller_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(controller_ray, out RaycastHit target, 50)) {
+            agent.SetDestination(target.point);
+        }
+    }
+}
