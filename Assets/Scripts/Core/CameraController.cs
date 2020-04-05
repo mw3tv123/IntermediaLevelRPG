913e9c54901eb8d7d5653e8cc131d94660924f9b
Hung <mw3tv123@gmail.com>
2020-04-05 16:00:32 +0700

    Basic movement and project structure.

---
+++ Assets/Scripts/Core/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core {
+    public class CameraController : MonoBehaviour {
+        [SerializeField]
+        private Transform target;
+        
+        // Update is called once per frame
+        private void LateUpdate ( ) {
+            transform.position = target.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Movement {
+    public class Mover : MonoBehaviour {
+        [SerializeField]
+        private Transform target;
+
+        private NavMeshAgent agent;
+
+        private void Start ( ) {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        private void Update ( ) {
+            if ( Input.GetMouseButton(0) ) {
+                MoveToCursor();
+            }
+            UpdateAnimator();
+        }
+
+        private void MoveToCursor ( ) {
+            // Get a ray from camera to mouse point.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // Casting the ray to a hit.
+            bool isHit = Physics.Raycast(ray, out RaycastHit hit);
+            if ( isHit ) {  // If the ray hit something...
+                agent.destination = hit.point;
+            }
+        }
+
+        private void UpdateAnimator ( ) {
+            Vector3 velocity = agent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+
+            GetComponent<Animator>().SetFloat("Speed", speed);
+        }
+    }
+}
